de6a8b7665e13d0d7dc5fdea4f52c98660727d6f
bileseomedia <sabilaseomedia@gmail.com>
2026-07-28 16:09:51 +0700

    Create  tailwind.config.js

---
+++ app/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#B76E79',
+        secondary: '#F5E6DA',
+        accent: '#8C3A3A',
+      },
+    },
+  },
+  plugins: [],
+}
